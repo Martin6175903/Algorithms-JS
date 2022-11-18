cb463b30c0dd9a82eeea9a9e2318f7cb81e48474
Martin6175903 <martin6175903@yandex.ru>
2022-11-19 00:25:53 +0300

    added file js with algorithms save cash with big volume data

---
+++ 11_cash.js
@@ -0,0 +1,37 @@
+// Алгоритм кеширования каких-то данных во избежания повторных вычислений какой-либо функции.
+
+// Создадим функции, которая будет кэшировать внутри себя результат выполнения другой либо функции.
+
+function cashFunction(fn) {
+    const cash = {};
+    return function (n) {
+        if (cash[n]) {
+            console.log('Взято из кэша', cash[n]);
+            return cash[n];
+        }
+        let result = fn(n);
+        console.log('Посчитала функция = ', result);
+        cash[n] = result;
+        return result;
+    }
+}
+
+function factorial(n) {
+    let result = 1;
+    while (n != 1) {
+        result *= n;
+        n--;
+    }
+    return result;
+}
+
+const cashFactorial = cashFunction(factorial);
+
+
+cashFactorial(5);
+cashFactorial(4);
+cashFactorial(3);
+cashFactorial(4);
+cashFactorial(5);
+cashFactorial(1);
+
